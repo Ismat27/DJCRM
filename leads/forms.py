7dd88ea7d950dfd8a93e6c7dbdc633217dd215f6
Ismat27 <46388260+Ismat27@users.noreply.github.com>
2022-02-01 15:03:42 +0100

    create new and logics

---
+++ leads/forms.py
@@ -0,0 +1,18 @@
+from re import L
+from django import forms
+from .models import Lead
+
+class ModelLeadForm(forms.ModelForm):
+    class Meta:
+        model = Lead
+        fields = (
+            'first_name',
+            'last_name',
+            'age',
+            'agent'
+        )
+
+class LeadForm(forms.Form):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    age = forms.IntegerField(min_value=0)
--- leads/urls.py
+++ leads/urls.py
@@ -1,6 +1,8 @@
-from django.http import path
-from . import views 
-
+from django.urls import path
+from .views import lead_list, lead_detail, lead_create
+app_name = 'leads'
 urlpatterns = [
-    
+    path("", lead_list),
+    path("<int:pk>/", lead_detail),
+    path("create/", lead_create)
 ]

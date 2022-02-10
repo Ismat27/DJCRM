c35e93fbd91b1f84b06cfab439474eea3ab883fb
Ismat27 <46388260+Ismat27@users.noreply.github.com>
2022-02-10 08:00:23 +0100

    use class based views

--- leads/urls.py
+++ leads/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import lead_list, lead_detail, lead_create, lead_update, lead_delete
+from . import views
 app_name = 'leads'
 urlpatterns = [
-    path("", lead_list),
-    path("<int:pk>/", lead_detail),
-    path("<int:pk>/update/", lead_update),
-    path("<int:pk>/delete/", lead_delete),
-    path("create/", lead_create),
+    path("", views.LeadListView.as_view(), name="lead_list"),
+    path("<int:pk>/", views.LeadDetailView.as_view(), name="lead_detail"),
+    path("<int:pk>/update/", views.LeadUpdateView.as_view(), name="lead_update"),
+    path("<int:pk>/delete/", views.LeadDeleteView.as_view(), name="lead_delete"),
+    path("create/", views.LeadCreateView.as_view(), name="lead_create"),
 ]

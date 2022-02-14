eacb1508794db24dd4cc31d1f9ca3785e5780b3e
Ismat27 <46388260+Ismat27@users.noreply.github.com>
2022-02-14 23:21:34 +0100

    overhaul database

--- leads/models.py
+++ leads/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     pass
 
 class UserProfile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self) :
         return self.user.username 

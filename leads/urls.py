68628d353b0350e44b658f70c81f22a96c133f79
Ismat27 <46388260+Ismat27@users.noreply.github.com>
2022-02-20 16:14:53 +0100

    implement functionality for assigning lead to agent

--- leads/urls.py
+++ leads/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("<int:pk>/", views.LeadDetailView.as_view(), name="lead_detail"),
     path("<int:pk>/update/", views.LeadUpdateView.as_view(), name="lead_update"),
     path("<int:pk>/delete/", views.LeadDeleteView.as_view(), name="lead_delete"),
+    path("<int:pk>/assign_agent/", views.AssignAgentView.as_view(), name="assign_agent"),
     path("create/", views.LeadCreateView.as_view(), name="lead_create"),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,9 +1,11 @@
 from multiprocessing import context
+from tempfile import template
+from tkinter import N
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from .models import Agent, Lead
-from .forms import LeadForm, ModelLeadForm, CustomUserCreationForm
+from .forms import LeadForm, ModelLeadForm, CustomUserCreationForm, AssignAgentForm
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -72,17 +74,6 @@ class LeadCreateView(LoginRequiredMixin, generic.CreateView):
         )
         return super(LeadCreateView, self).form_valid(form) 
 
-def lead_create(request):
-    form = ModelLeadForm()
-    if request.method == "POST":
-        form = ModelLeadForm(request.POST)
-        if form.is_valid():
-           form.save()
-        return redirect('/leads')
-    context = {
-        'form': form 
-    }
-    return render(request, 'leads/lead_create.html', context)
 
 class LeadUpdateView(LoginRequiredMixin, generic.UpdateView):
     template_name = 'leads/lead_update.html'
@@ -97,6 +88,50 @@ class LeadUpdateView(LoginRequiredMixin, generic.UpdateView):
         return reverse('leads:lead_list')
 
 
+class LeadDeleteView(LoginRequiredMixin, generic.DeleteView):
+    template_name = 'leads/lead_delete.html'
+
+    def get_queryset(self) :
+        user = self.request.user
+        queryset = Lead.objects.filter(organisation=user.userprofile)
+        return queryset
+
+    def get_success_url(self) :
+        return reverse('leads:lead_list')
+
+class AssignAgentView(LoginRequiredMixin, generic.FormView):
+    template_name = "leads/assign_agent.html"
+    form_class = AssignAgentForm
+
+    def get_form_kwargs(self, **kwargs):
+        kwargs = super(AssignAgentView, self).get_form_kwargs(**kwargs)
+        kwargs.update({
+            'request':self.request
+        })
+        return kwargs
+    
+    def form_valid(self, form):
+        agent = form.cleaned_data['agent']
+        lead = Lead.objects.get(id=self.kwargs['pk'])
+        lead.agent = agent
+        lead.save()
+        return super(AssignAgentView, self).form_valid(form)
+
+    def get_success_url(self) :
+        return reverse('leads:lead_list')
+
+def lead_create(request):
+    form = ModelLeadForm()
+    if request.method == "POST":
+        form = ModelLeadForm(request.POST)
+        if form.is_valid():
+           form.save()
+        return redirect('/leads')
+    context = {
+        'form': form 
+    }
+    return render(request, 'leads/lead_create.html', context)
+
 def lead_update(request, pk):
     lead = Lead.objects.get(id=pk)
     form = ModelLeadForm(instance=lead)
@@ -110,17 +145,6 @@ def lead_update(request, pk):
         'form': form
     }
     return render(request, 'leads/lead_update.html', context)
-
-class LeadDeleteView(LoginRequiredMixin, generic.DeleteView):
-    template_name = 'leads/lead_delete.html'
-
-    def get_queryset(self) :
-        user = self.request.user
-        queryset = Lead.objects.filter(organisation=user.userprofile)
-        return queryset
-
-    def get_success_url(self) :
-        return reverse('leads:lead_list')
 
 def lead_delete(request, pk):
     lead = Lead.objects.get(id=pk)

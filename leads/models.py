c07cade2e57fe49122e88c8d75a8d933dc651865
Ismat27 <46388260+Ismat27@users.noreply.github.com>
2022-02-20 17:50:31 +0100

    created the Category model and linked with Lead model

--- leads/models.py
+++ leads/models.py
@@ -1,4 +1,5 @@
 from tkinter import CASCADE
+from unicodedata import name
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
@@ -19,7 +20,8 @@ class Lead(models.Model):
     last_name = models.CharField(max_length=50)
     age = models.IntegerField(default=0)
     organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.CASCADE)
+    agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey("Category", null=True, blank=True, on_delete=models.SET_NULL) 
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -29,7 +31,13 @@ class Agent(models.Model):
     organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
     def __str__(self) :
-        return self.user.username 
+        return self.user.username
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.name 
 
 def user_created_signal(sender, instance, created, **kwargs):
     print(instance, created)

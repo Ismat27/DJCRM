95a76b83dff96edc25278861d1d9e783bbb0fc93
Ismat27 <46388260+Ismat27@users.noreply.github.com>
2022-02-20 13:51:48 +0100

    update LeadListView to identify unassigned leads

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
@@ -25,11 +26,21 @@ class LeadListView(LoginRequiredMixin, generic.ListView):
     def get_queryset(self) :
         user = self.request.user
         if user.is_organiser:
-            queryset = Lead.objects.filter(organisation=user.userprofile)
+            queryset = Lead.objects.filter(organisation=user.userprofile, agent__isnull=False)
         else: 
-            queryset = Lead.objects.filter(organisation=user.agent.organisation)
+            queryset = Lead.objects.filter(organisation=user.agent.organisation, agent__isnull=False)
             queryset = Lead.objects.filter(agent__user = user)
         return queryset
+
+    def get_context_data(self, **kwargs):
+        user = self.request.user
+        context = super(LeadListView, self).get_context_data(**kwargs)
+        if user.is_organiser:
+            queryset = Lead.objects.filter(organisation=user.userprofile, agent__isnull=True)
+            context.update({
+                'unassigned_leads':queryset
+            })
+        return context
 
 class LeadDetailView(LoginRequiredMixin, generic.DetailView):
     template_name = 'leads/lead_detail.html'
